edb09910345487e5b73c612b5a7bdaf279659412
Alequin <j-cox03@hotmail.com>
2021-09-26 16:39:19 +0100

    made a simple prototype of the app as a proof expo has the required features

---
+++ src/mocks/fake-api-call.js
@@ -0,0 +1,141 @@
+export const fakeApiCall = async () => mockData.videos;
+
+const mockData = {
+  videos: [
+    {
+      id: "j6RkC8hlaMk",
+      channelTitle: "Ceres Fauna Ch. hololive-EN",
+      title:
+        "【Fauna&#39;s ASMR】 Ear Cleaning, Oil Massage, &amp; Comfy ASMR Triggers 🌿 #holoCouncil",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/j6RkC8hlaMk/default.jpg",
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/j6RkC8hlaMk/mqdefault.jpg",
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/j6RkC8hlaMk/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+    },
+    {
+      id: "2hgwPt5kmZc",
+      channelTitle: "Ceres Fauna Ch. hololive-EN",
+      title:
+        "【Fauna&#39;s ASMR】 Cozy Autumn ASMR 🍂 Crinkley &amp; Comfy #holoCouncil",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/2hgwPt5kmZc/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/2hgwPt5kmZc/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/2hgwPt5kmZc/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+    },
+    {
+      id: "le9AAt8B5Gc",
+      channelTitle: "Ceres Fauna Ch. hololive-EN",
+      title:
+        "【ASMR】 Fauna&#39;s first ASMR stream! 🌿 comfy whispers and assorted ASMR triggers  #holoCouncil",
+
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/le9AAt8B5Gc/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/le9AAt8B5Gc/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/le9AAt8B5Gc/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+    },
+    {
+      id: "xd5p2MH1tiQ",
+      channelTitle: "Mori Calliope Ch. hololive-EN",
+      title:
+        "【ASMR】Whispering and Chatting in my Indoor Voice! #hololiveEnglish",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/xd5p2MH1tiQ/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/xd5p2MH1tiQ/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/xd5p2MH1tiQ/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+    },
+    {
+      id: "Qj5XO2TEa2U",
+      channelTitle: "Mori Calliope Ch. hololive-EN",
+      title:
+        "【ASMR DRAWING】Sketching ASMR on my Matte Tablet! Featuring... Cute Girls! #hololiveEnglish #holoMyth",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/Qj5XO2TEa2U/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/Qj5XO2TEa2U/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/Qj5XO2TEa2U/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+    },
+    {
+      id: "df3foPky_iE",
+      channelTitle: "Mori Calliope Ch. hololive-EN",
+      title:
+        "【LATE NIGHT雑談】Sleepy Reaper ASMR (?) and Free Talk! #HoloMyth #HololiveEnglish",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/df3foPky_iE/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/df3foPky_iE/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/df3foPky_iE/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+    },
+  ],
+};
--- src/navigation.js
+++ src/navigation.js
@@ -1,8 +1,13 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Dimensions, Image, Text, View } from "react-native";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { WebView } from "react-native-webview";
+import { fakeApiCall } from "./mocks/fake-api-call";
+
+const windowWidth = Dimensions.get("window").width;
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +20,7 @@ export const Navigation = () => {
           component={HomeView}
           options={{ headerShown: false }}
         />
+        <Stack.Screen name="videoView" component={VideoView} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -28,7 +34,63 @@ const FullScreenView = (props) => {
 const HomeView = () => {
   return (
     <FullScreenView>
-      <Text>Hello world</Text>
+      <ListOfVideos />
     </FullScreenView>
+  );
+};
+
+const ListOfVideos = () => {
+  const nav = useNavigation();
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fakeApiCall().then(setVideos);
+  });
+
+  const numColumns = 2;
+  return (
+    <FlatList
+      style={{ width: "100%", height: "100%" }}
+      data={videos}
+      numColumns={numColumns}
+      keyExtractor={({ id }) => id}
+      renderItem={({ item }) => (
+        <VideoButton
+          size={windowWidth / numColumns}
+          title={item.title}
+          thumbnails={item.thumbnails}
+          onSelectVideo={() =>
+            console.log("on click") ||
+            nav.navigate("videoView", { videoId: item.id })
+          }
+        />
+      )}
+    />
+  );
+};
+
+const VideoButton = ({ title, thumbnails, size, onSelectVideo }) => {
+  return (
+    <TouchableOpacity
+      style={{ width: size, height: size }}
+      onPress={onSelectVideo}
+    >
+      <Image
+        style={{ width: "100%", height: "100%", resizeMode: "stretch" }}
+        source={{ uri: thumbnails.medium.url }}
+      />
+    </TouchableOpacity>
+  );
+};
+
+const VideoView = ({ route }) => {
+  return (
+    <WebView
+      style={{ width: "100%", height: "100%" }}
+      originWhitelist={["*"]}
+      source={{
+        uri: `https://www.youtube.com/embed/${route.params.videoId}?rel=0&autoplay=0&showinfo=0&controls=1&start=60`,
+      }}
+    />
   );
 };
