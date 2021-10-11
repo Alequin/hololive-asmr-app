5f781b7e98f29bfcae93d0d4d9755b51ef4d62dd
Alequin <j-cox03@hotmail.com>
2021-10-12 00:59:17 +0100

    added icon and updated app config

--- app.config.js
+++ app.config.js
@@ -1,32 +1,29 @@
 const version = 2;
 
 export default {
-  name: "hololive-asmr",
-  slug: "hololive-asmr",
+  name: "Hololive ASMR Catalog",
+  slug: "hololive-asmr-catalog",
   version: `${version}.0.0`,
   orientation: "landscape",
   icon: "./assets/images/icon.png",
   scheme: "myapp",
   userInterfaceStyle: "automatic",
-  splash: {
-    image: "./assets/images/splash.png",
-    resizeMode: "contain",
-    backgroundColor: "#ffffff",
-  },
   updates: {
     fallbackToCacheTimeout: 0,
   },
   assetBundlePatterns: ["**/*"],
-  ios: {
-    supportsTablet: true,
-  },
   android: {
     adaptiveIcon: {
       foregroundImage: "./assets/images/adaptive-icon.png",
       backgroundColor: "#ffffff",
     },
   },
-  web: {
-    favicon: "./assets/images/favicon.png",
+  android: {
+    package: "com.just_for_fun.hololive_asmr_catalog",
+    versionCode: version,
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#000000",
+    },
   },
 };
