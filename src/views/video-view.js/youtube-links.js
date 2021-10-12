7780b3c7ec4ab6141d989592a5ea674155df4bed
Alequin <j-cox03@hotmail.com>
2021-10-12 16:14:50 +0100

    used locale to set the language for the embedded youtube player

--- src/views/video-view.js/youtube-links.js
+++ src/views/video-view.js/youtube-links.js
@@ -1,4 +1,5 @@
 import * as Linking from "expo-linking";
+import * as Localization from "expo-localization";
 
 export const toYoutubeChannel = (channelId) =>
   Linking.openURL(`https://www.youtube.com/channel/${channelId}`);
@@ -6,7 +7,7 @@ export const toYoutubeChannel = (channelId) =>
 // embedded video docs - https://developers.google.com/youtube/player_parameters
 const dontAutoPlayVideo = "autoplay=0";
 const shouldShowVideoControls = "controls=1";
-const interfaceLanguage = `hl=en`; // TODO - set this based on device locale
+const interfaceLanguage = `hl=${Localization.locale}`; // TODO - set this based on device locale
 const hideFullScreenOption = "fs=0";
 const EMBEDDED_PLAYER_CONFIG_QUERY_STRING = `${dontAutoPlayVideo}&${shouldShowVideoControls}&${interfaceLanguage}&${hideFullScreenOption}`;
 
