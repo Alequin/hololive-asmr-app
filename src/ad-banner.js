0d303e9af95fdf841699c4275f29d8fea1c2ccff
Alequin <j-cox03@hotmail.com>
2021-10-23 15:40:52 +0100

    ad banner now attempts to reset if there was an error but the app state changes

--- src/ad-banner.js
+++ src/ad-banner.js
@@ -1,25 +1,35 @@
 import { AdMobBanner } from "expo-ads-admob";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import { isEnvironmentProduction } from "./environment";
 import secrets from "./secrets";
+import { useIsAppStateActive } from "./use-app-state";
 
 export const AdBanner = () => {
+  const isAppActive = useIsAppStateActive();
   const [shouldShowAd, setShouldShowAd] = useState(true);
+
+  useEffect(() => {
+    if (isAppActive && !shouldShowAd) setShouldShowAd(true);
+  }, [isAppActive]);
 
   return (
     <>
       <View style={{ width: "100%", height: 1, backgroundColor: "white" }} />
-      <AdMobBanner
-        style={!shouldShowAd ? { height: 0 } : { alignItems: "center" }}
-        bannerSize="smartBanner"
-        servePersonalizedAds={false}
-        adUnitID={
-          isEnvironmentProduction() ? secrets.addBannerId : "ca-app-pub-3940256099942544/6300978111"
-        }
-        onAdViewDidReceiveAd={() => setShouldShowAd(true)}
-        onDidFailToReceiveAdWithError={() => setShouldShowAd(false)}
-      />
+      {shouldShowAd && (
+        <AdMobBanner
+          style={{ alignItems: "center" }}
+          bannerSize="smartBanner"
+          servePersonalizedAds={false}
+          adUnitID={
+            isEnvironmentProduction()
+              ? secrets.addBannerId
+              : "ca-app-pub-3940256099942544/6300978111"
+          }
+          onAdViewDidReceiveAd={() => setShouldShowAd(true)}
+          onDidFailToReceiveAdWithError={() => setShouldShowAd(false)}
+        />
+      )}
     </>
   );
 };
